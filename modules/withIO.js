5c69b82121228909ab495e8098b36ca330401324
Liam Mitchell <liam.mitchell@sharecare.com>
2017-04-25 14:38:46 +0200

    Update withIO, forgot in refactoring

--- modules/withIO.js
+++ modules/withIO.js
@@ -5,32 +5,25 @@ import values from 'lodash/values'
 import mapValues from 'lodash/mapValues'
 import zipObject from 'lodash/zipObject'
 import {combineLatest} from 'rxjs/observable/combineLatest'
-import reject from './reject'
 
 export default function withIO(urls) {
-  if (!isPlainObject(urls)) {
-    throw new Error('withIO requires a map of io urls')
-  }
+  return source => request => {
+    const {io, method} = request
 
-  return function mergeIOintoRequest(request, source) {
-    const {io, method, single = false} = request
+    const urlsMap = typeof urls === 'function' ?
+      urls(request) :
+      urls
 
-    // Throw error if url property name clashes with request property.
-    if (keys(urls).some(k => request.hasOwnProperty(k))) {
-      const key = keys(urls).find(k => request.hasOwnProperty(k))
-      return reject(request, `withIO property name ${key} clashes with request property`)
+    if (!isPlainObject(urlsMap)) {
+      throw new Error('withIO requires a map of io objects/requests')
     }
 
-    const ioRequests = mapValues(urls, url => {
-      url = typeof url === 'string' ? {path: url} : url
-      return io({
-        ...url,
-        // Only allow observe.
-        method: 'OBSERVE',
-        // Single read if it's not observable or if single flag is set.
-        single: single || method !== 'OBSERVE'
-      })
-    })
+    // Turn urls into observables if they aren't already.
+    const ioRequests = mapValues(urlsMap, url =>
+      typeof url.subscribe === 'function' ?
+        url :
+        io(url)
+    )
 
     const continueRequestWithValues = values =>
       source({
