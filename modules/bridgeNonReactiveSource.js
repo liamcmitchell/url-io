e4285a23a1eacbb6a151a11621e250b62a9ea236
Liam Mitchell <git@liam.geek.nz>
2018-04-01 13:28:16 +0200

    Refactor bridgeNonReactiveSource

--- modules/bridgeNonReactiveSource.js
+++ modules/bridgeNonReactiveSource.js
@@ -26,32 +26,20 @@ const defaultRequestCacheInvalidationIterator = ({path, method}) => {
 }
 
 // Converts observes to gets and manages cache.
-export const bridgeNonReactiveSource = (options = {}) => {
-  const {
-    cache = {},
-    // request -> request
-    observeToReadRequest = defaultObserveToReadRequest,
-    // request -> falsy/int
-    requestCacheTime = defaultRequestCacheTime,
-    // request -> string
-    requestCacheKey = defaultRequestCacheKey,
-    // request -> fn(string -> bool)
-    requestCacheInvalidationIterator = defaultRequestCacheInvalidationIterator,
-  } = options
+export const bridgeNonReactiveSource = ({
+  cache = {},
+  // request -> request
+  observeToReadRequest = defaultObserveToReadRequest,
+  // request -> falsy/int
+  requestCacheTime = defaultRequestCacheTime,
+  // request -> string
+  requestCacheKey = defaultRequestCacheKey,
+  // request -> fn(string -> bool)
+  requestCacheInvalidationIterator = defaultRequestCacheInvalidationIterator,
+} = {}) => (source) => {
+  source = createSafeSource(source)
 
   const expiredKeys$ = new Subject()
-
-  const add = (key, value, expiry) => {
-    // May already have been added, remove older value.
-    if (cache[key]) {
-      clearTimeout(cache[key].expireTimeoutId)
-    }
-    cache[key] = {
-      value,
-      // Set timeout to remove itself on expiry.
-      expireTimeoutId: setTimeout(() => remove(key), expiry),
-    }
-  }
 
   const remove = (key) => {
     clearTimeout(cache[key].expireTimeoutId)
@@ -59,73 +47,76 @@ export const bridgeNonReactiveSource = (options = {}) => {
     expiredKeys$.next(key)
   }
 
-  const clear = (iterator) => {
-    Object.keys(cache)
-      .filter(iterator)
-      .forEach((key) => remove(key))
-  }
+  const doRequest = (request) => {
+    const expires = requestCacheTime(request)
+    const key = requestCacheKey(request)
+    const cacheInvalidationIterator = requestCacheInvalidationIterator(request)
+    const canCache = expires && !cacheInvalidationIterator
 
-  return (source) => {
-    source = createSafeSource(source)
-
-    const doRequest = (request) => {
-      const cacheTime = requestCacheTime(request)
-      const cacheKey = requestCacheKey(request)
-      const cacheInvalidationIterator = requestCacheInvalidationIterator(
+    if (expires && cacheInvalidationIterator) {
+      console.warn(
+        'Requests that mutate/invalidate cache cannot be cached',
         request
       )
-      const canCache = cacheTime && !cacheInvalidationIterator
-
-      if (cacheTime && cacheInvalidationIterator) {
-        console.warn(
-          'Requests that mutate/invalidate cache cannot be cached',
-          request
-        )
-      }
-
-      // Return from cache if possible.
-      if (canCache && cache.hasOwnProperty(cacheKey)) {
-        return Promise.resolve(cache[cacheKey].value)
-      }
-
-      const result = source(request)
-
-      // Add to cache if possible.
-      if (canCache) {
-        result.then((value) => add(cacheKey, value, cacheTime))
-      }
-
-      // Invalidate cache if required.
-      if (cacheInvalidationIterator) {
-        result
-          // Ignore error, clearing cache is conservative option.
-          .catch(() => {})
-          .then(() => clear(cacheInvalidationIterator))
-      }
-
-      return result
     }
 
-    return createSafeSource((request) => {
-      const {method} = request
+    // Return from cache if possible.
+    if (canCache && cache.hasOwnProperty(key)) {
+      return Promise.resolve(cache[key].value)
+    }
 
-      if (method === 'OBSERVE') {
-        const key = requestCacheKey(request)
+    const result = source(request)
 
-        const read = () =>
-          cache.hasOwnProperty(key)
-            ? // Get directly from cache if available.
-              of(cache[key].value)
-            : // Or read immediately.
-              doRequest(observeToReadRequest(request), source)
+    // Add to cache if possible.
+    if (canCache) {
+      result.then((value) => {
+        // May already have been added, remove older value.
+        if (cache[key]) {
+          clearTimeout(cache[key].expireTimeoutId)
+        }
 
-        return merge(
-          read(),
-          expiredKeys$.pipe(filter((k) => k === key), switchMap(read))
-        )
-      } else {
-        return doRequest(request, source)
-      }
-    })
+        cache[key] = {
+          value,
+          // Set timeout to remove itself on expiry.
+          expireTimeoutId: setTimeout(() => remove(key), expires),
+        }
+      })
+    }
+
+    // Invalidate cache if required.
+    if (cacheInvalidationIterator) {
+      result
+        // Ignore error, clearing cache is conservative option.
+        .catch(() => {})
+        .then(() => {
+          Object.keys(cache)
+            .filter(cacheInvalidationIterator)
+            .forEach(remove)
+        })
+    }
+
+    return result
   }
+
+  return createSafeSource((request) => {
+    const {method} = request
+
+    if (method === 'OBSERVE') {
+      const key = requestCacheKey(request)
+
+      const read = () =>
+        cache.hasOwnProperty(key)
+          ? // Get directly from cache if available.
+            of(cache[key].value)
+          : // Or send read request.
+            doRequest(observeToReadRequest(request))
+
+      return merge(
+        read(),
+        expiredKeys$.pipe(filter((k) => k === key), switchMap(read))
+      )
+    } else {
+      return doRequest(request)
+    }
+  })
 }
